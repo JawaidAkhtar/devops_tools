d4ac6e33750c7074eff60cfe90e83e8ff40c868b
Jawaid Akhtar <139637049+JawaidAkhtar@users.noreply.github.com>
2024-11-24 18:16:01 +0530

    Update minikube_installation.sh
    Updated Installing command of kubectl

--- minikube_installation.sh
+++ minikube_installation.sh
@@ -22,8 +22,8 @@ curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
 #downloading kubectl binary
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 
 #installing kubectl
